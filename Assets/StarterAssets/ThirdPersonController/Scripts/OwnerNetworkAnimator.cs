26dbfc88daeef40b644c4e32ac9dabde4bd84b81
Kretoskim <98364505+kretoskim@users.noreply.github.com>
2026-01-13 14:31:42 +0300

    integrated animated character and successfully synchronized

---
+++ Assets/StarterAssets/ThirdPersonController/Scripts/OwnerNetworkAnimator.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using Unity.Netcode.Components;
+
+    public class OwnerNetworkAnimator : NetworkAnimator
+    {
+        protected override bool OnIsServerAuthoritative()
+        {
+            return false;
+        }
+    }
+
