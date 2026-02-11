c67d5d3c510ab34f3cf7442dbd98284d25d62bf9
Kretoskim <98364505+kretoskim@users.noreply.github.com>
2026-02-11 14:32:33 +0300

    creating lobby for multiplayer -> lobby lasts beyond 30s inactive use

---
+++ Assets/Scripts/TestLobby.cs
@@ -0,0 +1,76 @@
+using System;
+using QFSW.QC;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using Unity.Services.Lobbies;
+using Unity.Services.Lobbies.Models;
+using UnityEngine;
+
+public class TestLobby : MonoBehaviour
+{
+    private Lobby hostLobby;
+    private float heartbeatTimer;
+   private async void Start() 
+   {
+        await UnityServices.InitializeAsync();
+        AuthenticationService.Instance.SignedIn += () => { Debug.Log("Signed in" + AuthenticationService.Instance.PlayerId);};
+
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
+   }
+    private void Update()
+    {
+        HandleLobbyHearbeat();
+    }
+
+    private async void HandleLobbyHearbeat()
+    {
+        if(hostLobby != null)
+        {
+            heartbeatTimer -= Time.deltaTime;
+            if(heartbeatTimer < 0f)
+            {
+                float heartbeatTimermax = 15;
+                heartbeatTimer = heartbeatTimermax;
+
+                await LobbyService.Instance.SendHeartbeatPingAsync(hostLobby.Id);
+            }
+        }
+    }
+
+    [Command] 
+   private async void CreateLobby()
+    {
+        try
+        {
+            string lobbyName = "MyLobby";
+            int maxPlayers = 4;
+            Lobby lobby = await LobbyService.Instance.CreateLobbyAsync(lobbyName, maxPlayers);
+
+            Debug.Log("Created Lobby " + lobby.Name + " " + lobby.MaxPlayers);
+        }
+        catch(LobbyServiceException e)
+        {
+            Debug.Log(e);
+        }
+    }
+
+    [Command]
+    private async void ListLobbies()
+    {
+        try
+        {
+            QueryResponse queryResponse = await LobbyService.Instance.QueryLobbiesAsync();
+
+            Debug.Log("Lobbies found: " + queryResponse.Results.Count);
+            foreach(Lobby lobby in queryResponse.Results)
+            {
+                Debug.Log(lobby.Name + " " + lobby.MaxPlayers);
+            }
+        }
+        
+        catch (LobbyServiceException e)
+        {
+            Debug.Log(e);
+        }
+    }
+}
